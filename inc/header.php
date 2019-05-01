300be956060a2f417798b1bcf909af06e359a9ce
dielanophals <43468300+dielanophals@users.noreply.github.com>
2019-05-01 14:27:28 +0200

    added basic header

---
+++ inc/header.php
@@ -0,0 +1,19 @@
+<header>
+  <nav>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Projects</a></li>
+      <li><a href="#">Organisations</a></li>
+      <li><a href="#">About us</a></li>
+    </ul>
+
+    <span class="nav_line">|</span>
+
+    <div class="nav_profile">
+      <div class="nav_profile_img">
+        <img src="https://scontent.fbru2-1.fna.fbcdn.net/v/t1.0-9/36490118_1784065068352595_989980852102365184_n.jpg?_nc_cat=107&_nc_ht=scontent.fbru2-1.fna&oh=0dba5b59c5b82897d8f56dd0f18b51dc&oe=5D683018" alt="">
+      </div>
+      <a href="#">Dielan Ophals</a>
+    </div>
+  </nav>
+</header>
--- index.php
+++ index.php
@@ -5,8 +5,10 @@
   <head>
     <meta charset="utf-8">
     <title></title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/styles.css">
   </head>
   <body>
-
+    <?php require_once("inc/header.php"); ?>
   </body>
 </html>
