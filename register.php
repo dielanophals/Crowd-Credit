b8127fac975a25ebb6736c6e1f784319229f3a1f
dielanophals <43468300+dielanophals@users.noreply.github.com>
2019-05-04 14:46:06 +0200

    created register

--- register.php
+++ register.php
@@ -6,16 +6,25 @@
   }
 
   if(!empty($_POST)){
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
     $email = $_POST['email'];
     $password = $_POST['password'];
+    $confirm_password = $_POST['confirm_password'];
 
-    $u = new User();
-    $isLogged = $u->login($email, $password);
+    $check_mail = User::checkMail($email);
 
-    if($isLogged){
-      header("Location: index.php");
+    if($check_mail == 0){
+      if($password == $confirm_password){
+        $hash_password = Security::hash($password);
+        User::register($firstname, $lastname, $email, $hash_password);
+        User::login($email, $password);
+        header("Location: index.php");
+      }else{
+        $error = "Your passwords don't match.";
+      }
     }else{
-      $err = true;
+      $error = "That email address is already in use.";
     }
   }
 
@@ -31,16 +40,16 @@
     <?php require_once("inc/header.php"); ?>
     <form action="#" method="post">
       <?php
-      if(isset($err)){
-        echo "<p class='error_message'>Je gegevens kloppen niet.</p>";
+      if(isset($error)){
+        echo "<p class='error_message'>$error</p>";
       }
       ?>
       <input type="text" name="firstname" placeholder="First name" required>
       <input type="text" name="lastname" placeholder="Last name" required>
-      <input type="email" name="email" placeholder="Email Adress" required>
+      <input type="email" name="email" placeholder="Email Address" required>
       <input type="password" name="password" placeholder="Password" required>
       <input type="password" name="confirm_password" placeholder="Confirm password" required>
-      <input class="red_btn btn" type="submit" name="submit" value="Login">
+      <input class="red_btn btn" type="submit" name="submit" value="Register">
       <p>Already have an account?</p>
       <a href="login.php">Sign in here</a>
     </form>
