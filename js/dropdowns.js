4aa7386a90ba8df8e588d667088b0feeb058f95c
Dielan <dielano@hotmail.be>
2019-05-07 13:09:29 +0200

    Update dropdowns.js

--- js/dropdowns.js
+++ js/dropdowns.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
   $(document).on("click","body", function (event) {
-     var target = event.target.id;
-     if(target == ""){
-       $(".nav_profile_dropdown").addClass("hide");
-       $(".dropdown").addClass("hide");
-     }
+    var target = event.target.id;
+    if(target == ""){
+      $(".nav_profile_dropdown").addClass("hide");
+      $(".dropdown").addClass("hide");
+    }
   });
 
-  $(document).on("click", ".nav_profile_dropdown_btn", function() {
+  $(document).on("click", ".nav_profile_dropdown_btn", function(e) {
+    e.preventDefault();
     $(".nav_profile_dropdown").toggleClass("hide");
   });
 
