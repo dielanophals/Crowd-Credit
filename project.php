2559f6305b885b6f9fdd85a12a7b4cd37c4a1444
Dielan <dielano@hotmail.be>
2019-05-08 11:03:01 +0200

    get location from project

--- project.php
+++ project.php
@@ -5,8 +5,9 @@
 
   if(isset($_GET['project'])){
     $project_id = $_GET['project'];
-    $project = new Project;
-    $project = $project->getProject($project_id);
+    $pro = new Project;
+    $project = $pro->getProject($project_id);
+    $location = $pro->getLocation($project['locations_id']);
   }else{
     header("Location: index.php");
   }
@@ -24,10 +25,14 @@
   <body>
     <?php require_once("inc/header.php"); ?>
     <main class="main_project">
-    <div class="container project">
+    <div class="container">
       <div class="banner_wrapper">
         <div class="banner_image" style="background:url(<?php echo $project['banner']; ?>); background-size:cover; background-position:center;"></div>
       </div>
+      <section class="project_information">
+        <h3 class="red"><?php echo $project['name']; ?></h3>
+        <h4><?php echo $location['continent']; ?></h4>
+      </section>
     </div>
     </main>
   </body>
