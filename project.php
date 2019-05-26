fb3ef56ae7fc1fd966ca22af42d072de80db9c8a
YannisWS <yannis.wellemans@hotmail.com>
2019-05-26 18:04:23 +0200

    Update CSS + include/head

--- project.php
+++ project.php
@@ -18,15 +18,7 @@
   }
 ?><!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Crowd Credit</title>
-    <script src="https://code.jquery.com/jquery-latest.min.js"></script>
-    <link href="https://fonts.googleapis.com/css?family=Nunito:400,700|Roboto" rel="stylesheet">
-    <script src="js/dropdowns.js"></script>
-    <script src="js/projects.js"></script>
-    <link rel="stylesheet" href="css/styles.css">
-  </head>
+  <?php require_once("inc/head.php"); ?>
   <body>
   <?php
     if(isset($_GET['fund']) && $_GET['fund'] == true):
