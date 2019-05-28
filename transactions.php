5c456cde667ccd35414c3fb08475676662a2f0ac
dielanophals <43468300+dielanophals@users.noreply.github.com>
2019-05-28 23:40:31 +0200

    show every transaction

--- transactions.php
+++ transactions.php
@@ -9,9 +9,19 @@
   <?php require_once("inc/header.php"); ?>
   <main>
     <div class="container">
+      <div class="transaction_title">
+        <h5>Project</h5>
+        <h5>Amount</h5>
+        <h5>Date</h5>
+      </div>
     <?php foreach(User::getUserTransactions() as $t): ?>
+      <?php $date = substr($t['timestamp'], 0, 10); ?>
+      <?php $project = Project::getProject($t['project_id']) ?>
+
       <div class="transaction">
-        <?php echo $t['amount']; ?>
+        <h4><?php echo $project['name']; ?></h4>
+        <h4>- €<?php echo $t['amount']; ?></h4>
+        <h4><?php echo $date; ?></h4>
       </div>
     <?php endforeach; ?>
     </div>
