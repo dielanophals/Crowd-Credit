53cb3025482abffb22839358bf97550941f53bc1
dielanophals <43468300+dielanophals@users.noreply.github.com>
2019-05-28 02:33:35 +0200

    added dark overlay to banner

--- index.php
+++ index.php
@@ -36,10 +36,12 @@
   <body id="page_index">
     <?php require_once("inc/header.php"); ?>
     <section class="banner_image">
-      <div class="info center">
-        <h1>Crowdfund a dream</h1>
-        <p>We are a crowdfunding platform for developing countries in the South. We want to give every person an equal chance</p>
-        <a class="btn red_btn" href="#">View projects</a>
+      <div class="overlay">
+        <div class="info center">
+          <h1>Crowdfund a dream</h1>
+          <p>We are a crowdfunding platform for developing countries in the South. We want to give every person an equal chance</p>
+          <a class="btn red_btn" href="#">View projects</a>
+        </div>
       </div>
     </section>
     <main class="main_home">
