5fed893ba89e80dd1c03eccb8ab2021625158c79
dielanophals <43468300+dielanophals@users.noreply.github.com>
2019-05-30 23:08:15 +0200

    Update project.php

--- project.php
+++ project.php
@@ -59,7 +59,9 @@
             <div class="progress" style="width: <?php echo $total_transactions_procent ?>%"></div>
           </div>
           <p class="lightgrey"><?php echo $total_transactions_procent; ?> %</p>
-          <a class="btn red_btn" href="?project=<?php echo $project_id; ?>&fund=true">Fund today</a>
+          <?php if($userData['organisation_id'] == 1): ?>
+            <a class="btn red_btn" href="?project=<?php echo $project_id; ?>&fund=true">Fund today</a>
+          <?php endif; ?>
         </div>
       </section>
     </div>
