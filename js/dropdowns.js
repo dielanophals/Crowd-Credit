15a32d76729a4371566afec12baa114ebbdb0407
dielanophals <43468300+dielanophals@users.noreply.github.com>
2019-06-02 21:21:03 +0200

    new db

--- js/dropdowns.js
+++ js/dropdowns.js
@@ -90,7 +90,7 @@ $(document).ready(function() {
             'amount': amount
         },
         success: function (data) {
-          alert(data)
+          window.location = "index.php";
         }
       });
     }else{
